a74a58da3c70c0cf06ee27b29c4ba425bdc293e8
daivanduong <dai22992@gmail.com>
2023-06-25 09:58:46 +0700

    Initial Commit

---
+++ MoviesFavorite/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesFavorite
+//
+//  Created by Ocean97 on 25/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
